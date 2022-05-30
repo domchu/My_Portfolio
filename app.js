350d42b2a94e4fbaecfbf021132942621085e7f1
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-05-30 23:18:59 +0100

    added page loader

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const bars = document.querySelector(".bars");
 const mobileView = document.querySelector(".mobile-view");
+const close = document.querySelector(".times");
 bars.addEventListener("click", () => {
   if (bars.classList.contains("fa-bars")) {
     mobileView.style.display = "block";
-    bars.classList.add("fa-times");
-    bars.classList.remove("fa-bars");
-  } else {
-    mobileView.style.display = "none";
-    bars.classList.add("fa-bars");
-    bars.classList.remove("fa-times");
+    // bars.classList.add("fa-times");
+    // bars.classList.remove("fa-bars");
   }
+  // else {
+  //   mobileView.style.display = "none";
+  //   bars.classList.add("fa-bars");
+  //   bars.classList.remove("fa-times");
+  // }
+});
+close.addEventListener("click", () => {
+  mobileView.style.display = "none";
 });
 // end of the bars toogle
+const pageLoader = document.querySelector(".page-loader");
+window.addEventListener("load", () => {
+  pageLoader.style.display = "none";
+});
+// end of the page loader
 
 let testimonial = document.querySelectorAll(".testimonial");
 
